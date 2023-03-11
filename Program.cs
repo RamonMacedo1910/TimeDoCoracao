7ac91d049aa6d13ff12c2ac12e07ec72be4353be
Ramon Mesquita <ramonmesquitamacedo2222@gmail.com>
2023-03-10 22:39:12 -0300

    time do coração

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿
+Console.ForegroundColor = ConsoleColor.Black;
+Console.BackgroundColor = ConsoleColor.White;
+Console.Write(" c o r ");
+
+Console.ForegroundColor = ConsoleColor.White;
+Console.BackgroundColor = ConsoleColor.Black;
+Console.Write(" i n ");
+
+Console.ForegroundColor = ConsoleColor.Black;
+Console.BackgroundColor = ConsoleColor.White;
+Console.Write(" t h i ");
+
+Console.ForegroundColor = ConsoleColor.White;
+Console.BackgroundColor = ConsoleColor.Black;
+Console.Write(" a n s ");
